b75a46f4497d178c6e5cb2c51f26555174e3c553
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-08-06 12:13:22 -0500

    Completed Assignment
    Completed task. Learned about networking tab and disabling cache.  This was causing issues with css styling.

--- dojo.py
+++ dojo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,5 +7,11 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def result():
-    return render_template('result.html')
+    your_name = request.form['your_name']
+    dojo_location = request.form['dojo_location']
+    fav_language = request.form['favorite_language']
+    your_comment = request.form['comment']
+    print your_name, dojo_location, fav_language, your_comment
+    return render_template('result.html', name=your_name, location=dojo_location,
+    language=fav_language, comment=your_comment)
 app.run(debug=True)
